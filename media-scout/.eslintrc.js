48078fdf687869145919e9a5ed35b1a50ea4d5ce
maxmeyaev <maxmeyaev05@gmail.com>
2023-02-15 21:03:24 -0800

    add eslint rules

---
+++ media-scout/.eslintrc.js
@@ -0,0 +1,30 @@
+/* eslint-disable quote-props */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'react/self-closing-comp': [
+      'error', {
+        component: true,
+        html: true
+      }
+    ]
+  }
+};
